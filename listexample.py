758850532d92f958968d4cae87c52cc71c8ee369
Sumagowdav <sgowdruv@gmail.com>
2025-02-23 16:24:21 +0530

    update details on code

--- listexample.py
+++ listexample.py
@@ -1,3 +1,6 @@
+# this below program fetch the user and login name who made pull request on kubernetes open repository
+# whenever you use response.json() it converst the json format data into dictnories
+
 import requests
 response =requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
 #print(response.json())
